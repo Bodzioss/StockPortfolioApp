8348a64f9403d981d77d20db5d961c26f4302182
Bodzioss <marcin_bogdanski1@wp.pl>
2022-12-27 22:43:57 +0100

    Add PortfolioComponent, StockData and Transaction models

---
+++ Models/Transaction.cs
@@ -0,0 +1,11 @@
+﻿namespace StockPortfolioApp.Models
+{
+    public class Transaction
+    {
+        public int Id { get; set; }
+        public int PortfolioId { get; set; }
+        public int StockId { get; set; }
+        public Decimal Value { get; set; }
+        public int Amount { get; set; }
+    }
+}
