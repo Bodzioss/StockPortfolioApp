d175f1da4282f41aa105f6191db9c30cc9e115ad
Bodzioss <marcin_bogdanski1@wp.pl>
2022-12-27 23:21:31 +0100

    Add StockExchange model

---
+++ Models/StockExchange.cs
@@ -0,0 +1,9 @@
+﻿namespace StockPortfolioApp.Models
+{
+    public class StockExchange
+    {
+        public int Id { get; set; }
+        public String? Name { get; set; }
+        public String? Country { get; set; }
+    }
+}
