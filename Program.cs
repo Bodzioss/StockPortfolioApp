5cb180dafb90b55ac69dd027089917df8243731c
Bodzioss <marcin_bogdanski1@wp.pl>
2023-01-04 23:39:36 +0100

    Add DbContext

--- Program.cs
+++ Program.cs
@@ -1,16 +1,19 @@
 global using StockPortfolioApp.Models;
 global using AutoMapper;
+global using Microsoft.EntityFrameworkCore;
 using StockPortfolioApp.Services.PortfolioComponentService;
 using StockPortfolioApp.Services.PortfolioService;
 using StockPortfolioApp.Services.StockDataService;
 using StockPortfolioApp.Services.StockService;
 using StockPortfolioApp.Services.TransactionService;
 using StockPortfolioApp.Services.UserService;
+using StockPortfolioApp.Services.StockExchangeService;
+global using StockPortfolioApp.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddDbContext<DataContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -18,6 +21,7 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<IPortfolioComponentService, PortfolioComponentService>();
 builder.Services.AddScoped<IPortfolioService, PortfolioService>();
 builder.Services.AddScoped<IStockDataService, StockDataService>();
+builder.Services.AddScoped<IStockExchangeService, StockExchangeService>();
 builder.Services.AddScoped<IStockService, StockService>();
 builder.Services.AddScoped<ITransactionService, TransactionService>();
 builder.Services.AddScoped<IUserService, UserService>();
